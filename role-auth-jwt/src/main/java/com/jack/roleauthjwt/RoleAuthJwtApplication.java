0d7b985657966b10e74ebdcfa4615a811118cba9
9gerald-s <119358670+9gerald-s@users.noreply.github.com>
2023-02-18 10:36:34 +0530

    Add files via upload

---
+++ role-auth-jwt/src/main/java/com/jack/roleauthjwt/RoleAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.jack.roleauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleAuthJwtApplication.class, args);
+	}
+
+}
---
+++ role-auth-jwt/src/test/java/com/jack/roleauthjwt/RoleAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.roleauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
